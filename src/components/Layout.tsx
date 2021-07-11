d4beca90b224505c237228a22dc315d0c111ba89
Pedro Moreno <nennogabriel@gmail.com>
2021-07-10 21:13:18 -0300

    form test

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function Layout({ children }) {
   return (
-    <Box maxW="1120px" wx="auto" p={4}>
+    <Box maxW="1120px" mx="auto" p={4}>
       <Center as="head">
         <img src="/img/logo-verbasa.svg" alt="Verbasa logo" width="180px" />
       </Center>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,29 @@
+import { Button, Input } from '@chakra-ui/react';
+import { Form } from '@unform/web';
 import Head from 'next/head';
-import React from 'react';
+import React, { useRef } from 'react';
 import Layout from '../components/Layout';
 
 export default function Home() {
-  return [
-    <Head key="head">
-      <title>Verbasa app</title>
-    </Head>,
-    <Layout key="HomePage">hello mama</Layout>,
-  ];
+  const formRef = useRef();
+
+  function handleFormSubmit(data) {
+    console.log(data);
+    return;
+  }
+
+  return (
+    <>
+      <Head key="head">
+        <title>Verbasa app</title>
+      </Head>
+
+      <Layout key="HomePage">
+        <Form ref={formRef} onSubmit={handleFormSubmit}>
+          <Input name="name" placeholder="Choose a username" />
+          <Button type="submit">Save</Button>
+        </Form>
+      </Layout>
+    </>
+  );
 }
