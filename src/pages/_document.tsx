59de75e074bc672fdfd8fbca33ceecc00af4f5fb
Pedro Moreno <nennogabriel@gmail.com>
2021-07-20 13:09:08 -0300

    fix: crosorigin atribute

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,11 @@ export default class Document extends NextDocument {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;700;900&display=swap"
             rel="stylesheet"
